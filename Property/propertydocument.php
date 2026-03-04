ce0079a5fff0c0895233d2f63db4d6900010b4e9
pibi2116 <camillephoebetlegaspi@gmail.com>
2026-03-04 09:59:11 +0800

    commit now

--- Property/propertydocument.php
+++ Property/propertydocument.php
@@ -1,42 +1,5 @@
-<!DOCTYPE html>
-<link rel="icon" href="PSAU.ico">
-<html>
-<head>
- <title>PSAU</title>
- <!--
- <p style="background-image: url('Head.jpg');background-repeat: no-repeat">        
- <script src='https://code.jquery.com/jquery-2.1.3.min.js'></script>
--->
-
-
-
-<div>
-<table>
-<tr>
-<td>
-<picture>
-  <img src="PSAU_10.jpg" alt="" style="width:auto;">
-</picture>
-</td>
-<td valign="top">
-    
-<label style="font-family:calibri;font-size:28px;" d >PAMPANGA STATE AGRICULTURAL UNIVERSITY</label>
-<hr style='border-top: 2px solid black'>
-<label style="font-family:calibri;font-size:24px;" >PROPERTY MANAGEMENT SYSTEM</label>
-</td>
-
-</tr>
-
-<body onload=loaduploadlist()></body>
-</head>
-</html>
-
 <?php include "connect.php";
 error_reporting(0);
-//$servername = "";
-//$username = "";
-//$password = "";
-//$dbname = "";
 $datatable = "property_list"; // MySQL table name
 $results_per_page = 27; // number of results per page
  
@@ -49,155 +12,196 @@ if ($conn->connect_error) {
 $filtertext="";
 ?>
 
-<form action='receiveadd.php' method='GET'>
-<table border='0'  width='100%'>
-<tr>
-<td align='center'><input hidden id='filtertext' name='filtertext' type='text' text-align='center' placeholder='Document Title / Document Type' value='' size='48'>   <button hidden type='submit'>SEARCH</button></td>
-</tr>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Property Details - PSAU</title>
+    <link rel="icon" href="PSAU.ico">
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="header">
+        <div class="header-content">
+            <img src="PSAU_10.jpg" alt="PSAU Logo" class="header-logo">
+            <div class="header-title">
+                <h1>PAMPANGA STATE AGRICULTURAL UNIVERSITY</h1>
+                <h2>Property Management System</h2>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+        <nav style="margin-bottom: 2rem;">
+            <a href="index.php" class="btn btn-primary">← Back to Property List</a>
+        </nav>
 
 <?php
-$filtertext=$_GET['filtertext'];
-$filtertext=trim($filtertext);
+$filtertext = isset($_GET['filtertext']) ? trim($_GET['filtertext']) : '';
 if (isset($_GET["page"])) { $page = $_GET["page"]; } else { $page=1; };
 $start_from = ($page-1) * $results_per_page;
-$sql = "SELECT * FROM ".$datatable." where property_tag = '".$filtertext."' order by property_tag asc LIMIT $start_from, ".$results_per_page;
+$sql = "SELECT * FROM ".$datatable." WHERE property_tag = '".$filtertext."' ORDER BY property_tag ASC LIMIT $start_from, ".$results_per_page;
 $rs_result = $conn->query($sql);
-?>
 
-<?php
-$sql = "SELECT COUNT(property_tag) AS total FROM ".$datatable." where property_tag = '".$filtertext."' order by property_tag asc";
-$result = $conn->query($sql);
-$row = $result->fetch_assoc();
-$total_pages = ceil($row["total"] / $results_per_page); // calculate total pages with results
-//echo "<br>";
-//echo "<center>";
-//for ($i=1; $i<=$total_pages; $i++) { // print links for all pages
-//echo "<a href='viewgrid.php?filtertext=".$filtertext."&page=".$i."'";
-//if ($i==$page) 
-//echo " class='curPage'";
-//echo ">".$i."</a> ";
-//};
-
-?>
-
-
-
-
-
-
-<?php include 'connect.php';
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) 
-{
-  die("Connection failed: " . $conn->connect_error);
-}
-$ReferenceID=$filtertext;
-
-
-$query = "SELECT * FROM property_list where property_tag='$ReferenceID'";
+// Get property details
+$ReferenceID = $filtertext;
+$query = "SELECT * FROM property_list WHERE property_tag='$ReferenceID'";
 $result = mysqli_query($conn, $query);
-$fontadjust = "28px";
-$fontgrid = "42px";
-echo"<input hidden id='qr' value='$ReferenceID'/><br>";
-
-if ($result)
-{
-    $row = mysqli_num_rows($result);
-       if ($row)
-          {
-            $count=mysqli_num_rows($result);
-
-            echo "<table width='100%' border='0px solid green' align='left' style='font-size:10px','border-collapse:collapse','font-weight:bold'>";
-
-            while($data=mysqli_fetch_row($result))
-            {
-                $selected_status=$data[3];
-                if ($selected_status=="RELEASED") {$hideme="hidden";$showme="";}
-                if ($selected_status=="FOR RELEASING") {$hideme="";$showme="<br>";}
-            echo
-            "
-            <td>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='20%'>NUMBER</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[1]<br></td>
-
-            
-            </tr>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>PROPERTY TAG</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[2]<br></td>
-            </tr>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>ITEM</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[3]<br></td>
-            </tr>
-            </td>
-          
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>DESCRIPTION/MODEL NUMBER</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[4]<br></td>
-            </tr>
-            </td>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>SERIAL NUMBER</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[5]<br></td>
-            </tr>
-            </td>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>VALUE</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[6]<br></td>
-            </tr>
-            </td>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>ACQUISITION DATE</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[7]<br></td>
-            </tr>
-            </td>
-
-            <tr>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left' width='10%'>ACCOUNTABLE PERSON</td>
-            <td style='font-size:$fontadjust;background-color:white;color:black;font-family:tahoma;text-align:left; valign:top' width='auto' height='20'>: $data[8]<br></td>
-            </tr>
-            </td>
-
-            <iframe hidden frameborder='0' align='left' id='qrcode' src='' width='250' height='250'></iframe>
-
-            ";
-            } 
-            echo "</table>";      
-          }
-    mysqli_free_result($result);
-}
-mysqli_close($conn);
 ?>
 
+        <?php if ($result && mysqli_num_rows($result) > 0): ?>
+            <?php while($data = mysqli_fetch_row($result)): ?>
+                <?php
+                $selected_status = $data[16]; // property_status field
+                $is_released = stripos($selected_status, 'released') !== false;
+                $show_release_section = !$is_released;
+                ?>
+                
+                <div class="property-card">
+                    <div class="property-header">
+                        <h3>Property Details</h3>
+                        <p>Property Tag: <strong><?php echo htmlspecialchars($data[2]); ?></strong></p>
+                    </div>
+                    
+                    <div class="property-body">
+                        <div class="property-details">
+                            <div class="detail-item">
+                                <span class="detail-label">Property Number</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[1]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Property Tag</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[2]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Item</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[3]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Description/Model Number</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[4]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Serial Number</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[5]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Value</span>
+                                <span class="detail-value">₱<?php echo !empty($data[6]) ? number_format($data[6], 2) : '0.00'; ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Acquisition Date</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[7]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Accountable Person</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[8]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Actual Location</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[10]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Condition</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[14]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Status</span>
+                                <span class="detail-value">
+                                    <?php 
+                                    $status = htmlspecialchars($selected_status);
+                                    $status_class = "";
+                                    if (stripos($status, 'released') !== false) {
+                                        $status_class = "status-released";
+                                    } elseif (stripos($status, 'releasing') !== false) {
+                                        $status_class = "status-for-releasing";
+                                    }
+                                    echo "<span class='status-badge $status_class'>$status</span>";
+                                    ?>
+                                </span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Fund</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[17]); ?></span>
+                            </div>
+                            
+                            <div class="detail-item">
+                                <span class="detail-label">Year Purchased</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[18]); ?></span>
+                            </div>
+                            
+                            <?php if (!empty($data[11])): ?>
+                            <div class="detail-item" style="grid-column: 1 / -1;">
+                                <span class="detail-label">Remarks</span>
+                                <span class="detail-value"><?php echo htmlspecialchars($data[11]); ?></span>
+                            </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="qr-section">
+                    <h3>QR Code</h3>
+                    <div class="qr-code">
+                        <iframe frameborder='0' id='qrcode' src='' width='200' height='200'></iframe>
+                    </div>
+                    <p>Scan this QR code to view property details</p>
+                </div>
+                
+                <?php if ($show_release_section): ?>
+                <div class="property-card" style="margin-top: 2rem;">
+                    <div class="property-header" style="background: linear-gradient(135deg, var(--warning-color), #d97706);">
+                        <h3>Release Property</h3>
+                        <p>Process property release</p>
+                    </div>
+                    <div class="property-body">
+                        <form action='receiveadd.php' method='POST'>
+                            <div style="text-align: center; padding: 2rem;">
+                                <label for="ename" style="font-size: 1.2rem; font-weight: 600;">...</label>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <?php endif; ?>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <div class="property-card">
+                <div class="property-body" style="text-align: center; padding: 3rem;">
+                    <h3>Property Not Found</h3>
+                    <p style="color: var(--gray-500); margin-top: 1rem;">
+                        No property found with tag: <strong><?php echo htmlspecialchars($ReferenceID); ?></strong>
+                    </p>
+                    <a href="index.php" class="btn btn-primary" style="margin-top: 1rem;">← Back to Property List</a>
+                </div>
+            </div>
+        <?php endif; ?>
+        
+        <input type="hidden" id="qr" value="<?php echo $ReferenceID; ?>">
+    </div>
 
+    <footer style="text-align: center; padding: 2rem; color: var(--gray-500); margin-top: 3rem;">
+        <p>&copy; <?php echo date('Y'); ?> PAMPANGA STATE AGRICULTURAL UNIVERSITY - Property Management System</p>
+    </footer>
 
-
-
-
-<form action='receiveadd.php' method='POST'>
-    <label> </label>
-    <br>
- 
-<table>
-
-<tr>
-<td>
-<?php echo $showme; ?>
-<label <?php echo $hideme; ?> for="ename" style="font-size:42px">...</label>
-</td>
-</tr>
-
-<tr>
-<td>
-
-
-
+    <script>
+    function UpdateQRCode(val){
+        document.getElementById("qrcode").setAttribute("src","https://api.mimfa.net/qrcode?value="+encodeURIComponent(val)+"&as=value");
+    }
+    document.addEventListener("DOMContentLoaded", function(){
+        UpdateQRCode(document.getElementById("qr").value);
+    });
+    </script>
+</body>
+</html>
